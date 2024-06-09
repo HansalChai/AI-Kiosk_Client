b25101e450eb9e517f0e073457e5296d306a0887
ethankim77 <roki03@naver.com>
2024-06-09 10:46:16 +0900

    pay/creditcard pay/complete page (complex pages complete)

--- kiosk_user/src/app/(after ai cam)/complex/pay/complete/page.tsx
+++ kiosk_user/src/app/(after ai cam)/complex/pay/complete/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 
-import card from "@/../public/Credit Card.svg"
-import kakaopay from "@/../public/kakaopay.svg"
+import receipt from "@/../public/receipt.svg"
 
 export default function Complex() {
   return (
     <div className={styles.container}>
-      신용카드결제페이지
+      <div className={styles.title}>주문이 완료되었습니다</div>
+      <div className={styles.order}>주문번호</div>
+      <div className={styles.orderNum}>618</div>
+      <div className={styles.recieptment}>신용카드를 뽑은 후 출력된 영수증을 받아 가세요</div>
+      <Image src={receipt} alt="receipt img" className={styles.cardsvg}></Image>
     </div>
   );
 }
